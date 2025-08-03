5d8fd548de28b4542b4d4939d6d74f3fdc25d26f
Ademiday00 <128483939+Ademiday00@users.noreply.github.com>
2025-08-02 18:01:43 -0700

    updated -v

--- src/component/Firstpage.tsx
+++ src/component/Firstpage.tsx
@@ -3,7 +3,7 @@ const Firstpage = () => {
     <>
       <div className="d-flex justify-content-between align-items-center p-3 text-white">
         <h2 className="mb-0">Instagram</h2>
-        <div className="d-flex gap-5 fs-3">
+        <div className="d-flex gap-3 fs-3">
           <i className="far fa-heart"></i>
           <i className="fab fa-facebook-messenger"></i>
         </div>
